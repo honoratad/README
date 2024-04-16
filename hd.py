ef8728ca4f153d19aaad04dd9a07c2c99b8eff5d
honoratad <165071454+honoratad@users.noreply.github.com>
2024-04-16 19:07:21 +0200

    Add files via upload

---
+++ hd.py
@@ -0,0 +1,41 @@
+lager=['rzeski','jasny','lekki']
+pils = ['jasny','goryczkowy','srednio-mocny']
+porter=['gorzki','mocny','ciemny']
+stout=['ciemny','kawowy','bogaty']
+bezalkoholowe=['owowcowe','lekkie','bezalkoholowe']
+ale=['lekki','owocowy','kwiatowy']
+
+lista=[lager,pils,porter,strout,bezalkoholowe,ale]
+preferencja
+
+def lista:
+if (set(preferencja)
+    return "Sukces!!!"
+elif ''
+else:
+    return "Smutno =("
+
+print(sprawdz_piwo(lager))
+
+puby={
+    'Pub1':['zywiec','perla','konska dawka'],
+    'Pub2':['gaska beerbika','specjal'],
+    'Pub3':['balios','orkiszowe z czosnkiem','ucho od sledzia'],
+    'Pub4':['zywiec','fabrykanckie swinie']
+}
+
+moje_piwo='ucho od sledzia'
+
+nazwy_pub=list(puby.keys())
+znalezione = False
+index=0
+
+while index<len(nazwy_pub) and not znalezione:
+    aktpub= nazwy_pub[index]
+    if moje_piwo in puby[aktpub]:
+        znalezione= True
+        print('Pije piwo
+
+for wiek in range(18):
+    print(f'Masz {wiek} lat')
+    print('Mozesz kupic piwa')
